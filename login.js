f42430c0244632e44297fc7f98797db1a9b9d438
Aya.Ehab <aya.eek@gmail.com>
2024-06-10 09:37:52 +0400

    updat

--- login.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.toggle-password').addEventListener('click', function () {
-    const passwordInput = document.querySelector('#password');
-    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-    passwordInput.setAttribute('type', type);
-    this.textContent = type === 'password' ? '👁️' : '👁';
-});
